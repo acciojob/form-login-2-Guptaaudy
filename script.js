b81006c54d11c43879330e68ba438d0ba0346cf1
Uday Gupta <153746090+Guptaaudy@users.noreply.github.com>
2024-01-17 21:36:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+<script>
+    document.getElementById("myForm").addEventListener("submit", function(event) {
+      event.preventDefault(); // Prevents the form from submitting the traditional way
+
+      // Get values from form
+      var name = document.getElementById("name").value;
+      var LastName = document.getElementById("Last_name").value;
+      var PhoneNumber = document.getElementById("Phone_Number").value;
+      var email = document.getElementById("email").value;
+
+      // Display alert with form data
+      alert("Form submitted!\nName: " + name +"\nLast Name: " + LastName + "\nPhone Number: " + PhoneNumber +"\nEmail: " + email);
+    });
+  </script>
+
